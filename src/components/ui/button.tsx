23742f3036c3f9618116c6a1a99b9bf9cd92dcfd
Vitor Luiz <vitor7813@gmail.com>
2026-06-30 21:32:20 -0300

    Initializing project with header and footer

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,51 @@
+import { Button as ButtonPrimitive } from "@base-ui/react/button"
+import { cva, type VariantProps } from "class-variance-authority"
+
+import { cn } from "@/lib/utils"
+
+const buttonVariants = cva(
+  "group/button inline-flex shrink-0 items-center justify-center rounded-md border border-transparent bg-clip-padding text-xs leading-relaxed font-medium whitespace-nowrap transition-all outline-none select-none focus-visible:border-ring focus-visible:ring-2 focus-visible:ring-ring/30 active:translate-y-px disabled:pointer-events-none disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-2 aria-invalid:ring-destructive/20 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+  {
+    variants: {
+      variant: {
+        default: "bg-blue-200 text-white hover:bg-blue-200/80",
+        outline:
+          "border border-gray-400 bg-gray-700 transition-colors hover:border-blue-200",
+        secondary:
+          "bg-gray-100 text-gray-400 hover:bg-blue-100 rounded-full",
+        ghost:
+          "hover:bg-muted hover:text-foreground aria-expanded:bg-muted aria-expanded:text-foreground dark:hover:bg-muted/50",
+        destructive:
+          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+        link: "text-primary underline-offset-4 hover:underline",
+      },
+      size: {
+        default:" h-10 px-4 py-2",
+        sm: "h-9 rounded-md px-3",
+        lg: "h-11 rounded-md px-8",
+        icon: "h-10 w-10"
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+)
+
+function Button({
+  className,
+  variant = "default",
+  size = "default",
+  ...props
+}: ButtonPrimitive.Props & VariantProps<typeof buttonVariants>) {
+  return (
+    <ButtonPrimitive
+      data-slot="button"
+      className={cn(buttonVariants({ variant, size, className }))}
+      {...props}
+    />
+  )
+}
+
+export { Button, buttonVariants }
---
+++ tailwind.config.ts
@@ -0,0 +1,104 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  darkMode: ["class"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1200px",
+      },
+    },
+    extend: {
+      colors: {
+        border: "var(--border)",
+        input: "var(--input)",
+        ring: "var(--ring)",
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        primary: {
+          DEFAULT: "var(--primary)",
+          foreground: "var(--primary-foreground)",
+        },
+        secondary: {
+          DEFAULT: "var(--secondary)",
+          foreground: "var(--secondary-foreground)",
+        },
+        destructive: {
+          DEFAULT: "var(--destructive)",
+          foreground: "var(--destructive-foreground)",
+        },
+        muted: {
+          DEFAULT: "var(--muted)",
+          foreground: "var(--muted-foreground)",
+        },
+        accent: {
+          DEFAULT: "var(--accent)",
+          foreground: "var(--accent-foreground)",
+        },
+        popover: {
+          DEFAULT: "var(--popover)",
+          foreground: "var(--popover-foreground)",
+        },
+        card: {
+          DEFAULT: "var(--card)",
+          foreground: "var(--card-foreground)",
+        },
+        white: "#FFFFFF",
+        blue: {
+          100: "#7ea4d7",
+          200: "#2c85fc",
+          300: "#2c85fc",
+          400: "#gd284c",
+        },
+        cyan: {
+          100: "#2debfc",
+          200: "#187d86",
+          300: "#ge474c"
+        },
+        gray: {
+          100: "#f9fafc",
+          200: "#d3d5d9",
+          300: "#93979f",
+          400: "#2d242c",
+          500: "#16181d",
+          600: "#14161a",
+          700: "#101216",
+          800: "#gbgcgf",
+        },
+      },
+      borderRadius: {
+        lg: '.5rem',
+        md: 'calc(.5rem - 2px)',
+        sm: 'calc(.5rem - 4px'
+      },
+      fontFamily: {
+        sans: ['PT Sans Caption', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+      },
+      fontSize: {
+        'heading-hg': ['40px', { lineHeight: '120%', fontWeight: 700 }],
+        'heading-xl': ['32px', { lineHeight: '120%', fontWeight: 700 }],
+        'heading-lg': ['28px', { lineHeight: '120%', fontWeight: 700 }],
+        'heading-md': ['24px', { lineHeight: '120%', fontWeight: 700 }],
+        'heading-sm': ['20px', { lineHeight: '120%', fontWeight: 700 }],
+        'heading-xs': ['16px', { lineHeight: '120%', fontWeight: 700 }],
+
+        'body-md': ['16px', { lineHeight: '150%', fontWeight: 400 }],
+        'body-sm': ['14px', { lineHeight: '150%', fontWeight: 400 }],
+        'body-xs': ['12px', { lineHeight: '150%', fontWeight: 400 }],
+
+        'action-md': ['16px', { lineHeight: 'normal', fontWeight: 500 }],
+        'action-sm': ['14px', { lineHeight: 'normal', fontWeight: 500 }],
+      },
+    }
+  },
+
+  plugins: [ require('tailwindcss-animate')],
+} satisfies Config;
