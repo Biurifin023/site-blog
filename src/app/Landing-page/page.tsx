2247a8bcad1a01b9881a221a9f23b4e6e81524e2
Vitor Luiz <vitor7813@gmail.com>
2026-07-09 15:55:45 -0300

    Add SEO

--- src/app/Landing-page/page.tsx
+++ src/app/Landing-page/page.tsx
@@ -1,4 +1,26 @@
+import type { Metadata } from "next"
 import { LandingPage } from "@/templates/landing-page/landing-page"
+
+export const metadata: Metadata = {
+  title: 'Site.set',
+  description: 'Venda seus produtos como afiliado em um unico lugar.',
+  robots: 'index, follow',
+  openGraph: {
+    title: 'Site.set',
+    description: 'Venda seus produtos como afiliado em um unico lugar.',
+    url: 'https://site-blog-topaz.vercel.app/',
+    siteName: 'Site.set',
+    images: [
+      {
+        url: 'https://site-blog-topaz.vercel.app/og-image.png',
+        width: 800,
+        height: 600,
+        alt: 'Site.set',
+      },
+    ],
+  }
+
+}
 
 export default function LandingPageRoute() {
   return <LandingPage />
